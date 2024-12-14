b2c7cf666e8db629ff6ed8af1711440389b66dba
iveri99 <lukacho.iv@gmail.com>
2024-12-14 12:52:47 +0000

    Added list of words

--- hangman_game.py
+++ hangman_game.py
@@ -1,2 +1,12 @@
 import random
 
+word_list = [
+    "astronaut", "banana", "chocolate", "dinosaur", "elephant", "giraffe", "harbor", "igloo", "jungle",
+    "kitchen", "lemonade", "mountain", "notebook", "octopus", "pencil", "quilt", "robot", "sunflower",
+    "tornado", "umbrella", "vampire", "wizard", "xylophone", "yacht", "zebra", "adventure", "blueberry",
+    "courage", "diamond", "envelope", "frostbite", "guitar", "hurricane", "invisible", "jigsaw", "kaleidoscope",
+    "lighthouse", "marshmallow", "nebula", "opal", "puzzle", "quicksand", "rainbow", "sapphire", "tulip",
+    "unicorn", "vortex", "whisper", "xenon", "yellow", "zombie", "appreciate", "believe", "cobblestone",
+    "delicious", "elephant", "fascinate", "geometry", "heartbeat", "inspire", "journey", "knowledge",
+    "lemon", "mystery", "nostalgia", "oxygen", "paradox", "quicksilver", "resilience", "symphony", "treasure"
+]
